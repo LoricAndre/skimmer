bb5c44a3288948db2964831c4c14a16649a976fd
LoricAndre <loric.andre@pm.me>
2024-10-29 22:00:21 +0100

    chore: remove bitflags entirely

--- skimmer/src/util.rs
+++ skimmer/src/util.rs
@@ -3,6 +3,7 @@ use std::cmp::{max, min};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::prelude::v1::*;
+use std::str::FromStr;
 
 use regex::{Captures, Regex};
 use tuikit::prelude::*;
@@ -10,7 +11,6 @@ use unicode_width::UnicodeWidthChar;
 
 use crate::field::get_string_by_range;
 use crate::AnsiString;
-use bitflags::_core::str::FromStr;
 
 lazy_static! {
     static ref RE_ESCAPE: Regex = Regex::new(r"['\U{00}]").unwrap();
